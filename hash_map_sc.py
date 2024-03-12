7383373c72ca23b2349e1262eb54109f6ff82767
danimcbee23 <114363879+danimcbee23@users.noreply.github.com>
2024-03-11 21:04:38 -0400

    Update hash_map_sc.py

--- hash_map_sc.py
+++ hash_map_sc.py
@@ -119,33 +119,22 @@ class HashMap:
         if new_capacity < 1:
             return
 
-        # Confirm new_capacity is a prime number
         if not self._is_prime(new_capacity):
             new_capacity = self._next_prime(new_capacity)
 
-        # Create updated table with new capacity
         new_table = HashMap(new_capacity, self._hash_function)
 
-        # Rehash elements and put into new table
+        if new_capacity == 2:
+            new_table._capacity = 2
+
         for i in range(self._capacity):
             if self._buckets.get_at_index(i).length() > 0:
                 for element in self._buckets.get_at_index(i):
                     new_table.put(element.key, element.value)
 
-        # Update table attributes
-        self._buckets = DynamicArray()
-        new_table_capacity = new_table.get_capacity()
-
-        # Copy buckets from new_table
-        for i in range(new_table_capacity):
-            self._buckets.append(LinkedList())
-            curr_bucket = new_table._buckets.get_at_index(i)
-            for element in curr_bucket:
-                self._buckets[i].insert(element.key, element.value)
-
-        # Update pointers
+        self._buckets = new_table._buckets
         self._size = new_table._size
-        self._capacity = new_table_capacity
+        self._capacity = new_table.get_capacity()
 
     def table_load(self) -> float:
         """ Return the current load factor of hash table """
